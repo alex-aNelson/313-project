e2446da46140779bdb3a40bb342f0d4a3e1fbca9
Taya Morinville <149643813+tmorinville@users.noreply.github.com>
2024-04-26 10:25:13 -0500

    Added next and previous buttons to calendar to move between months

--- calendar_project/calendarAPP/views.py
+++ calendar_project/calendarAPP/views.py
@@ -3,9 +3,10 @@ from .models import Event
 from django.contrib.auth.models import User
 from datetime import date, datetime, timedelta
 from .utils import Calendar
-# import calendar
+import calendar
 from django.utils.safestring import mark_safe
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 def home(request):
@@ -26,19 +27,22 @@ def home(request):
 class CalendarView(generic.ListView):
     model = Event
     template_name = 'calendarAPP/calendar.html'
-
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
         # use today's date for the calendar
-        d = get_date(self.request.GET.get('day', None))
-
+        #d = get_date(self.request.GET.get('day', None))
+        d = get_date(self.request.GET.get('month', None))
+        
         # Instantiate our calendar class with today's year and date
         cal = Calendar(d.year, d.month)
 
         # Call the formatmonth method, which returns our calendar as a table
         html_cal = cal.formatmonth(withyear=True)
         context['calendar'] = mark_safe(html_cal)
+        context['prev_month'] = prev_month(d)
+        context['next_month'] = next_month(d)
         return context
 
 def get_date(req_day):
@@ -46,3 +50,16 @@ def get_date(req_day):
         year, month = (int(x) for x in req_day.split('-'))
         return date(year, month, day=1)
     return datetime.today()
+
+def prev_month(d):
+    first = d.replace(day=1)
+    prev_month = first - timedelta(days=1)
+    month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
+    return month
+
+def next_month(d):
+    days_in_month = calendar.monthrange(d.year, d.month)[1]
+    last = d.replace(day=days_in_month)
+    next_month = last + timedelta(days=1)
+    month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
+    return month
