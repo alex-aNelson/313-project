aee54dc98947d4348e34cc3c49de159fcb564430
ZacharyForkenbrock <159510103+ZacharyForkenbrock@users.noreply.github.com>
2024-04-26 17:14:12 -0500

    start is now up

--- calendar_project/calendarAPP/urls.py
+++ calendar_project/calendarAPP/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('calendar_project.urls')),
-    path('', include('django.contrib.auth.urls')),
+    path('', views.login, name='login'),
+
+    # path('', include('django.contrib.auth.urls')),
 ]
