dd5b197a728e549476a72cf66478a6f4fb23ab55
Taya Morinville <149643813+tmorinville@users.noreply.github.com>
2024-04-25 20:00:47 -0500

    Got initial calendar working, still working on a few changes

--- calendar_project/calendarAPP/urls.py
+++ calendar_project/calendarAPP/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'cal'
 urlpatterns = [
     path('', views.home, name='home'),
     # path('/add-event', views.add, name='add')
+    path('calendar/', views.CalendarView.as_view(), name='calendar'),
 ]
