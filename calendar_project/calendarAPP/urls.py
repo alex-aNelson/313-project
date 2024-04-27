8cc3df5842ddb6e43bd72bf83cabbcfdafa6477a
ZacharyForkenbrock <159510103+ZacharyForkenbrock@users.noreply.github.com>
2024-04-27 15:13:02 -0500

    changes 3:12 4/27/2024

--- calendar_project/calendarAPP/urls.py
+++ calendar_project/calendarAPP/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.login, name='login'),
-    
-    # path('', include('django.contrib.auth.urls')),
+    path('login/', views.login, name='login'),
+    path('sign-up/', views.signup, name='signup'),
+    path('', include('django.contrib.auth.urls')),
 ]
--- calendar_project/calendarAPP/views.py
+++ calendar_project/calendarAPP/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 
 def login(request):
     return render(request, "calendarAPP/login.html", {})
+
+# views.py
+from django.shortcuts import render
+
+
+def signup(request):
+    return render(request, 'sign-up.html')
