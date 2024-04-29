6716b307dff0ddfd655df1d85a6811dbe4c55985
alex-aNelson <159586652+alex-aNelson@users.noreply.github.com>
2024-04-29 14:25:38 -0500

    created the event details html

--- calendar_project/calendarAPP/views.py
+++ calendar_project/calendarAPP/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Event
 # Create your views here.
+
+def event_detail(request, pk):
+    event = Event.objects.get(pk = pk)
+    return render(request, 'calendarAPP/event_detail.html', {'event': event})
