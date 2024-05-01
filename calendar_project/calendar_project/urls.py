3ed61c3a5afb580e77676d1256552048f31a3433
alex-aNelson <159586652+alex-aNelson@users.noreply.github.com>
2024-05-01 12:03:45 -0500

    url

--- calendar_project/calendar_project/urls.py
+++ calendar_project/calendar_project/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('calendarAPP.urls'))
+    path('', include('calendarAPP.urls')),
+    # path('', include('calendarAPP.urls')),
+    # path('', views.login, name='login'),
+    # path('login/', views.login, name='login'),
+    # path('sign-up/', views.signup, name='signup'),
 ]
