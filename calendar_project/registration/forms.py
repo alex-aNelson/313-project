1ab16c208971e1439f8a30c38690196b76384958
ZacharyForkenbrock <159510103+ZacharyForkenbrock@users.noreply.github.com>
2024-05-01 11:38:16 -0500

    registration is displayed but button isnt mapped correctly

---
+++ calendar_project/registration/forms.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+class SignUpForm(UserCreationForm):
+    date_of_birth = forms.DateField()
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'date_of_birth', 'password1', 'password2']
+        
