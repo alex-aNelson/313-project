c25726a8cd17161d2fc5fa397273d0f9b2bc62a1
Taya Morinville <149643813+tmorinville@users.noreply.github.com>
2024-05-02 10:25:17 -0500

    Index page redirects to login if user is not authenticated

--- calendar_project/calendarAPP/views.py
+++ calendar_project/calendarAPP/views.py
@@ -16,19 +16,12 @@ from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    # if request.user.is_authenticated():
-    #     event_list = Event.objects.filter(users_event_id=request.user.id)
-    #     return render(request, 'calendarAPP/index.html', {'user_events' : event_list})
-    # else:
-    #     return redirect("home") # UPDATE TO LOGIN ONCE VIEW/URL IS MADE
-    # Query to get Events specific to user logged in, also filters out events that are outdated from current date
-    # event_list = Event.objects.filter(users_event_id=request.user.id, event_date__gt=date.today()).order_by("event_date") 
-    # return render(request, 'calendarAPP/index.html', {'user_events' : event_list})
-    
-    event_list_future = Event.objects.filter(users_event_id=request.user.id, event_date__date__gt=date.today()).order_by("event_date") 
-    event_list_today = Event.objects.filter(users_event_id=request.user.id, event_date__date=date.today()).order_by("event_date") 
-    
-    return render(request, 'calendarAPP/index.html', {'today_events' : event_list_today, 'future_events' : event_list_future})  
+    if request.user.is_authenticated:
+        event_list_future = Event.objects.filter(users_event_id=request.user.id, event_date__date__gt=date.today()).order_by("event_date") 
+        event_list_today = Event.objects.filter(users_event_id=request.user.id, event_date__date=date.today()).order_by("event_date") 
+        return render(request, 'calendarAPP/index.html', {'today_events' : event_list_today, 'future_events' : event_list_future})  
+    else:
+        return render(request, 'calendarAPP/login.html', {})
 
 class CalendarView(generic.ListView):
     model = Event
